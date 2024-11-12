298b2be4adeaf5899fdafad151f8eac9567aeb52
Joe <joe_ethan2@hotmail.com>
2024-11-12 23:23:05 +0000

    add: tierlist models created

---
+++ backend/models/tierlist.go
@@ -0,0 +1,29 @@
+package models
+
+type Tierlist struct {
+	ID int `gorm:"primaryKey" json:"id"`
+	Name string `json:"name"`
+	Description string `json:"description"`
+	CreatorID int `json:"creator_id"`
+	Creator User `gorm:"foreignKey:CreatorID"`
+	Tiers []Tiers `gorm:"foreignKey:TierlistID"`
+	Items []Items `gorm:"foreignKey:TierlistID"`
+	Version int `json:"version"`
+}
+
+type Tiers struct {
+	ID int `gorm:"primaryKey" json:"id"`
+	TierlistID int `json:"tierlist_id"`
+	Text string `json:"text"`
+	Colour string `json:"colour"`
+	Tierlist Tierlist `gorm:"foreignKey:TierlistID"`
+}
+
+type Items struct {
+	ID int `gorm:"primaryKey" json:"id"`
+	TierlistID int `json:"tierlist_id"`
+	Text string `json:"text"`
+	Image string `json:"image"`
+	TiersID int `json:"tier_id"`
+	Tier Tiers `gorm:"foreignKey:TierID"`
+}
