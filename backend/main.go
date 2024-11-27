f2682d5dc189eaaf5df9a45dc279793bf00dece4
Joe <joe_ethan2@hotmail.com>
2024-11-27 16:53:31 +0000

    add: utility function setup to not congest main.go

--- backend/main.go
+++ backend/main.go
@@ -4,13 +4,13 @@ import (
 	"github.com/gin-gonic/gin"
 
 	"tierlist/database"
-	"tierlist/routes"
+	"tierlist/utilities"
 )
 
 func main() {
 	database.ConnectDatabase()
 	database.Seed()
 	r := gin.Default()
-	routes.SetupUserRoutes(r)
+	utilities.SetupRoutes(r)
 	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
   }
